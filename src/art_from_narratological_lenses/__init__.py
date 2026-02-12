65da394f8baad010d289e4e8d7305dc533279580
--4444-j--99---- <padavano.anthony@gmail.com>
2026-02-12 08:31:55 -0500

    add package init

---
+++ src/art_from_narratological_lenses/__init__.py
@@ -0,0 +1,8 @@
+"""art-from-narratological-lenses: Narrative structures as visual art.
+
+Interactive web experience that transforms narrative theory from ORGAN-I
+into explorable visual compositions through algorithmic lenses.
+"""
+
+__version__ = "0.1.0"
+__author__ = "organvm-ii-poiesis"
